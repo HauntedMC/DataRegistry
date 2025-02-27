42db6001cdc7167b6a448115b66f27c96657a306
remdui <remyduijsens@gmail.com>
2025-02-27 22:26:04 +0100

    Fix dataprovider API calls

--- src/main/java/nl/hauntedmc/dataregistry/DataRegistry.java
+++ src/main/java/nl/hauntedmc/dataregistry/DataRegistry.java
@@ -1,6 +1,6 @@
 package nl.hauntedmc.dataregistry;
 
-import nl.hauntedmc.dataprovider.DataProvider;
+import nl.hauntedmc.dataprovider.BukkitDataProvider;
 import nl.hauntedmc.dataprovider.api.DataProviderAPI;
 import nl.hauntedmc.dataprovider.database.base.BaseDatabaseProvider;
 import nl.hauntedmc.dataprovider.database.DatabaseType;
@@ -22,16 +22,16 @@ public class DataRegistry extends JavaPlugin {
     @Override
     public void onDisable() {
         ormContext.shutdown();
-        dataProviderAPI.unregisterAllDatabases(this);
+        dataProviderAPI.unregisterAllDatabases(this.getName());
     }
 
     @Override
     public void onEnable() {
         instance = this;
 
-        dataProviderAPI = DataProvider.getDataProviderAPI();
-        dataProviderAPI.authenticate(this, "82cf4e2c-d67a-450c-a777-97298a404b73");
-        BaseDatabaseProvider provider = dataProviderAPI.registerDatabase(this, DatabaseType.MYSQL, "test_conn");
+        dataProviderAPI = BukkitDataProvider.getDataProviderAPI();
+        dataProviderAPI.authenticate(this.getName(), "c5c052c7-b1a3-4c58-8b04-78496b2d4bd8");
+        BaseDatabaseProvider provider = dataProviderAPI.registerDatabase(this.getName(), DatabaseType.MYSQL, "test_conn");
 
         if (provider == null || !provider.isConnected()) {
             getLogger().severe("Database connection not established, disabling plugin.");
@@ -43,7 +43,7 @@ public class DataRegistry extends JavaPlugin {
         RelationalDatabaseProvider relationalProvider = (RelationalDatabaseProvider) provider;
         DataSource dataSource = relationalProvider.getDataSource();
 
-        ormContext = new ORMContext(this, dataSource, GamePlayer.class);
+        ormContext = new ORMContext(this.getName(), dataSource, GamePlayer.class);
 
         PlayerService playerService = new PlayerService(this);
         getServer().getPluginManager().registerEvents(new PlayerListener(playerService), this);
