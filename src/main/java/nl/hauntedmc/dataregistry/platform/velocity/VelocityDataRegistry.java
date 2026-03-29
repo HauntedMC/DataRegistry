521756a78e90f605ffe794eb58081cb5cea67218
Remy Duijsens <remyduijsens@gmail.com>
2026-03-29 20:37:02 +0200

    Bump version to v1.7.3 for release

--- src/main/java/nl/hauntedmc/dataregistry/platform/velocity/VelocityDataRegistry.java
+++ src/main/java/nl/hauntedmc/dataregistry/platform/velocity/VelocityDataRegistry.java
@@ -52,7 +52,7 @@ import java.util.concurrent.atomic.AtomicReference;
 @Plugin(
         id = "dataregistry",
         name = "DataRegistry",
-        version = "1.7.2",
+        version = "1.7.3",
         description = "DataRegistry for cross-platform data handling.",
         authors = {"HauntedMC"},
         dependencies = @Dependency(id = "dataprovider")
