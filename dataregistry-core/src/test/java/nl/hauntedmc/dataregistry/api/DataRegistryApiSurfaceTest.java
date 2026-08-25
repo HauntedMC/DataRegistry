f4c24c388a1dbbf68e16d70cec70dd40cb5f22b0
Remy Duijsens <remyduijsens@gmail.com>
2026-08-25 11:33:18 +0200

    Verify platform instrumentation API exposure

--- dataregistry-core/src/test/java/nl/hauntedmc/dataregistry/api/DataRegistryApiSurfaceTest.java
+++ dataregistry-core/src/test/java/nl/hauntedmc/dataregistry/api/DataRegistryApiSurfaceTest.java
@@ -1,6 +1,8 @@
 package nl.hauntedmc.dataregistry.api;
 
+import nl.hauntedmc.dataregistry.api.observation.DataRegistryInstrumentation;
 import nl.hauntedmc.dataregistry.platform.common.PlatformPlugin;
+import nl.hauntedmc.dataregistry.platform.common.logger.ILoggerAdapter;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Method;
@@ -8,6 +10,9 @@ import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.withSettings;
 
 class DataRegistryApiSurfaceTest {
 
@@ -38,8 +43,37 @@ class DataRegistryApiSurfaceTest {
     }
 
     @Test
-    void platformPublishesTheNarrowApiRatherThanTheCoreRuntime() throws NoSuchMethodException {
+    void platformPublishesTheNarrowApiAndSeparateInstrumentationCapability() throws NoSuchMethodException {
         assertEquals(DataRegistryApi.class, DataRegistryApiProvider.class.getMethod("getDataRegistry").getReturnType());
         assertEquals(DataRegistryApi.class, PlatformPlugin.class.getMethod("getDataRegistry").getReturnType());
+        assertEquals(
+                DataRegistryInstrumentation.class,
+                DataRegistryApiProvider.class.getMethod("getDataRegistryInstrumentation").getReturnType()
+        );
+        assertEquals(
+                DataRegistryInstrumentation.class,
+                PlatformPlugin.class.getMethod("getDataRegistryInstrumentation").getReturnType()
+        );
+    }
+
+    @Test
+    void platformInstrumentationResolvesTheActiveRuntimeCapability() {
+        DataRegistryApi api = mock(
+                DataRegistryApi.class,
+                withSettings().extraInterfaces(DataRegistryInstrumentation.class)
+        );
+        PlatformPlugin plugin = new PlatformPlugin() {
+            @Override
+            public DataRegistryApi getDataRegistry() {
+                return api;
+            }
+
+            @Override
+            public ILoggerAdapter getPlatformLogger() {
+                return null;
+            }
+        };
+
+        assertSame((DataRegistryInstrumentation) api, plugin.getDataRegistryInstrumentation());
     }
 }
